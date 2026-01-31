27f002df888faf7d171ec3fb0d3741fb015052a2
Yousuf7900 <yousufislam7900@gmail.com>
2026-02-01 01:02:09 +0600

    delete and many more

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -79,8 +79,62 @@ async function run() {
             const result = await tutorialsCollection.find({ tutorEmail: email }).toArray();
             res.send(result);
         })
+        // get specific tutor detail by id
+        app.get('/tutor-details/:id', async (req, res) => {
+            const tutor_id = req.params.id;
+            const query = { _id: new ObjectId(tutor_id) };
+            const result = await tutorialsCollection.findOne(query);
+            res.send(result);
+        })
 
 
+
+        // bookings collection
+        const bookingsCollection = client.db('learnSphereDB').collection('bookings');
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        })
+
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const bookings = await bookingsCollection.find({ userEmail: email }).toArray();
+            if (bookings.length === 0) {
+                return res.send([]);
+            }
+            const tutorIds = bookings.map(b => new ObjectId(b.tutorId));
+            const tutors = await tutorialsCollection.find({ _id: { $in: tutorIds } }).toArray();
+            const result = bookings.map(b => {
+                const tutor = tutors.find(t => t._id.toString() === b.tutorId);
+                return {
+                    bookingId: b._id,
+                    tutorId: b.tutorId,
+                    name: tutor?.tutorName,
+                    image: tutor?.tutorPhoto,
+                    language: tutor?.language,
+                    price: tutor?.price,
+                    review: tutor?.review,
+                    tutorEmail: tutor?.tutorEmail,
+                    userEmail: b.userEmail
+                }
+            });
+            res.send(result);
+        })
+
+        app.patch('/tutors/review/:id', async (req, res) => {
+            const tutorId = req.params.id;
+            const result = await tutorialsCollection.updateOne({ _id: new ObjectId(tutorId) }, { $inc: { review: 1 } });
+            res.send(result);
+        })
+
+        app.delete('/tutorials/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await tutorialsCollection.deleteOne({
+                _id: new ObjectId(id)
+            });
+            res.send(result);
+        })
 
     } finally {
         // await client.close();
